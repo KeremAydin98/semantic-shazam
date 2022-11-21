ccc9f46afc8ec52cedc8c7f467d0743ae2c26b5c
KeremAydin98 <aydinke16@itu.edu.tr>
2022-11-21 11:27:33 +0300

    new summarizer

--- song_summarizer.py
+++ song_summarizer.py
@@ -1,6 +1,29 @@
 import config
 from models import *
 import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the data
+df = pd.read_csv("Data/combined-data.csv")
+df = df.dropna()
+
+# Extract the lyrics and summaries of lyrics
+song_lyrics = list(df["Lyrics"])
+song_summaries = list(df["Summary"])
+
+text_count = []
+summary_count = []
+
+for sent in df["Lyrics"]:
+    text_count.append(len(sent.split()))
+
+for sent in df["Summary"]:
+    summary_count.append(len(sent.split()))
+
+print(np.percentile(text_count, 95))
+print(np.percentile(summary_count, 95))
 
 # Text encoder stack
 encoder = Encoder(config.vocab_size, config.embedding_dim, config.enc_units, config.batch_size)
